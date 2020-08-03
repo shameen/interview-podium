88f7fff9893c23a3830b479a710793b82965abe2
Shameen <shameen.azeez@bookatable.com>
2020-08-03 12:47:40 +0100

    website: run `yarn` on app start to ensure packages installed

--- website/server.js
+++ website/server.js
@@ -5,8 +5,10 @@ const port = process.env.port || 1337;
 const app = new express();
 
 //Step 1: Build create-react-app
+//(Ideally we move this to a Post-build step)
 const execSync = require("child_process").execSync;
-execSync("npm run build");
+execSync("yarn"); //Install packages
+execSync("npm run build"); //Production build
 
 //Step 2: express
 app.use(express.static(path.join(__dirname, "build")));
