ddc4b4b8b96ccf2163cfd3f8208abda2462885f5
Shameen <shameen.azeez@bookatable.com>
2020-08-03 12:49:36 +0100

    Add random number generator

---
+++ MortgageApi/Logic/SecureIdGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace PodiumInterview.MortgageApi.Logic
+{
+    public interface IRandomNumberGenerator
+    {
+        long GetRandomLong();
+    }
+
+    /// <summary>
+    /// Uses <see cref="RNGCryptoServiceProvider"/> which is cryptographically secure, but not very fast.
+    /// </summary>
+    public class SecureIdGenerator : IRandomNumberGenerator
+    {
+        public long GetRandomLong()
+        {
+            using (var rng = new RNGCryptoServiceProvider())
+            {
+                byte[] bytes = new byte[64];
+                rng.GetBytes(bytes);
+                return Convert.ToInt64(bytes);
+            }
+        }
+    }
+}
