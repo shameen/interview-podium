d5ee0b0b91ae479310f1434d191fcf50db19a45a
Shameen <shameen.azeez@bookatable.com>
2020-08-04 14:22:00 +0100

    Database: Re-create DB on start-up

--- Database/PodiumDbContext.cs
+++ Database/PodiumDbContext.cs
@@ -6,6 +6,12 @@ namespace PodiumInterview.Database
 {
     public class PodiumDbContext : DbContext, IPodiumDatabaseContext
     {
+        public PodiumDbContext() : base()
+        {
+            //Re-create Database every time + Seed
+            Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
         public virtual DbSet<Applicant> Applicants { get; set; }
         public virtual DbSet<Lender> Lenders { get; set; }
         public virtual DbSet<MortgageProduct> MortgageProducts { get; set; }
